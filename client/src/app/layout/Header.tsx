cc5150d6a7418b70d85ed1da5c5ec483d315cd32
PraveenRoshane <praveenroshane123@gmail.com>
2022-01-05 00:18:58 -0800

    End of section 4

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,19 +1,78 @@
-import { AppBar, Switch, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { ShoppingCartRounded } from '@mui/icons-material'
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from '@mui/material'
+import { NavLink } from 'react-router-dom'
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
 
-export default function Header({darkMode, handleThemeChange}: Props) {
+// const midLinks = [
+//     { title: 'catalog', path: '/catalog' },
+//     { title: 'about', path: '/about' },
+//     { title: 'contact', path: '/contact' },
+// ]
+
+// const rightLinks = [
+//     { title: 'login', path: '/login' },
+//     { title: 'register', path: '/register' },
+// ]
+
+const navStyles = {
+    color: 'inherit',
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': { color: 'grey.500' },
+    '&.active': { color: 'black' }
+}
+
+
+export default function Header({ darkMode, handleThemeChange }: Props) {
     return (
-        <AppBar position='static' sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant='h6'>
-                    RE-STORE
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange}/>
+        <AppBar position='static' sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+
+                <Box display='flex' alignItems='center'>
+                    <Typography variant='h6' component={NavLink} to='/' sx={navStyles}>
+                        RE-STORE
+                    </Typography>
+                    <Switch checked={darkMode} onChange={handleThemeChange} />
+                </Box>
+
+
+                <List sx={{ display: 'flex' }}>
+                    <ListItem sx={navStyles} component={NavLink} to={'/catalog'}>
+                        CATALOG
+                    </ListItem>
+                    <ListItem sx={navStyles} component={NavLink} to={'/about'}>
+                        ABOUT
+                    </ListItem>
+                    <ListItem sx={navStyles} component={NavLink} to={'/contact'}>
+                        CONTACT
+                    </ListItem>
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' sx={{ color: 'inherit' }}>
+                        <Badge badgeContent={4} color='secondary'>
+                            <ShoppingCartRounded />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: 'flex' }}>
+                        <ListItem sx={navStyles}>
+                            <NavLink to={'/login'} style={{ color: 'inherit', textDecoration: 'none' }}>
+                                LOGIN
+                            </NavLink>
+                        </ListItem>
+                        <ListItem sx={navStyles}>
+                            <NavLink to={'/register'} style={{ color: 'inherit', textDecoration: 'none' }}>
+                                REGISTER
+                            </NavLink>
+                        </ListItem>
+                    </List>
+                </Box>
+
+
             </Toolbar>
         </AppBar>
     )
