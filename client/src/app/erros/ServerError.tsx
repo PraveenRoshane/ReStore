1fcd4426224cea069f3cbf9bf96936ded4974f77
PraveenRoshane <praveenroshane123@gmail.com>
2022-01-08 19:02:23 -0800

    End of section 5

---
+++ client/src/app/erros/ServerError.tsx
@@ -0,0 +1,10 @@
+import { Container, Paper, Typography } from '@mui/material'
+import React from 'react'
+
+export default function ServerError() {
+    return (
+        <Container component={Paper}>
+            <Typography>Server Error</Typography>
+        </Container>
+    )
+}
